62762562b6807a01553f28314b0356c2384b1e68
Nick Hathaway <Nicholashathaway@gmail.com>
2018-08-02 16:13:43 -0600

    set up sass

--- js/readings.js
+++ js/readings.js
@@ -1,17 +1,21 @@
-function fetchReadings() {
-  const Http = new XMLHttpRequest();
-  Http.open('GET', 'http://localhost:4500/api/readings');
-  Http.send();
-  Http.onreadystatechange = e => {
-    console.log(e);
-    // get rid of error here
-    let readingsJSON = JSON.parse(Http.response);
-    console.log(readingsJSON);
-  }
-}
+// function fetchReadings() {
+//   const Http = new XMLHttpRequest();
+//   Http.open('GET', 'http://localhost:4500/api/readings');
+//   Http.send();
+//   Http.onreadystatechange = e => {
+//     console.log(e);
+//     // get rid of error here
+//     let readingsJSON = JSON.parse(Http.response);
+//     console.log(readingsJSON);
+//   }
+// }
 
-function loadReadings() {
+// function loadReadings() {
 
-}
+// }
 
-fetchReadings();
+// fetchReadings();
+
+$(document).ready(function() {
+  $.ajax({ url: '',  })
+})
