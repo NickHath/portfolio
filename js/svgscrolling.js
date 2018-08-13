d2a5b916877e580a6eb74193380c0cec5fe8760a
Nick Hathaway <Nicholashathaway@gmail.com>
2018-08-13 13:52:27 -0600

    set up basic scroll-magic animation

---
+++ js/svgscrolling.js
@@ -0,0 +1,13 @@
+// $(document).ready(function() {
+//   var controller = new ScrollMagic.Controller();
+//   var svgDrawing = new ScrollMagic.Scene({
+//     triggerElement: '.svg-bg',
+//     // modify duration to be shorter than line height and the lines will be further down screen
+//     duration: $('.svg-bg').height() * 0.8,
+//     triggerHook: 0
+//   })
+//   .setTween(TweenMax.to('path', 1.5, {
+//     'stroke-dashoffset': '0'
+//   }))
+//   .addTo(controller);
+// });
