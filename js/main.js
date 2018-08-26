35767346a29246795c0d6f25326a412c4d6791e7
Nick Hathaway <Nicholashathaway@gmail.com>
2018-08-26 15:29:11 -0600

    add links for personal projects

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function($) {
+  // hamburger menu 
+  $('#hamburger-menu').click(function() {
+    $(this).toggleClass('is-active');
+  });
+  // collapsible sections
   $('.projects').find('.collapsible-toggle').click(function(){
     $(this).next().slideToggle('fast');
   });
