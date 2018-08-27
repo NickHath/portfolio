27f5f72be25d3bb209ac2cfc95d007aa285634a6
Nick Hathaway <Nicholashathaway@gmail.com>
2018-08-26 18:49:01 -0600

    style nav modal

--- js/main.js
+++ js/main.js
@@ -3,8 +3,4 @@ $(document).ready(function($) {
   $('#hamburger-menu').click(function() {
     $(this).toggleClass('is-active');
   });
-  // collapsible sections
-  $('.projects').find('.collapsible-toggle').click(function(){
-    $(this).next().slideToggle('fast');
-  });
 });
--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -4,10 +4,12 @@ var fadeOptions = {
   reset: false,
   distance: '80px',
   duration: 1400,
-  delay: 450,
+  // delay: 450,
+  delay: 250,
   scale: 0,
   origin: 'bottom'
 }
 
-sr.reveal('.experience', fadeOptions);
+// sr.reveal('.experience', fadeOptions);
 // sr.reveal('.subsection', fadeOptions);
+sr.reveal('.fadein', fadeOptions);
